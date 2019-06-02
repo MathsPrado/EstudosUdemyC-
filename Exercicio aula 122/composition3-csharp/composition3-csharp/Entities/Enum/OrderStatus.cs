e9e7a3aa84cab9fac708bdb30e4258c3c5530a74
unknown <matheusprado55@gmail.com>
2019-06-02 16:14:40 -0300

    exercicio

---
+++ Exercicio aula 122/composition3-csharp/composition3-csharp/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace composition3_csharp.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Exercicio aula 122/composition3-csharp/composition3-csharp/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace composition3_csharp.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
