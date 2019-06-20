bf56206397e26b5bc87296170f8ec28b86c1d377
unknown <matheusprado55@gmail.com>
2019-06-20 11:34:01 -0300

    Aula 196, interface, injecao de indepencia etc

---
+++ Exercicio Fixacaoo aula 196 interface/Exercicio Fixação aula 196 interface/Entities/Installment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercicio_Fixação_aula_196_interface.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio Fixacaoo aula 196 interface/Exercicio Fixação aula 196 interface/Services/PaypalService.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio_Fixação_aula_196_interface.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private const double FeePercentage = 0.02;
+
+        private const double MonthlyInterest = 0.01;
+
+        #region Metodos
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+        #endregion
+    }
+}
+
